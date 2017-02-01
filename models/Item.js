802367b4069f75d0bec5e08836ea6ae68ab14236
Russell Yeo <rusyeo@gmail.com>
2017-02-01 12:06:41 +0000

    add User model

--- models/Item.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const clothesSchema = new mongoose.Schema({
-  name: String,
-  size: String,
-  type: String,
-  material: String,
-  color: String
-});
---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type String, default: '' },
+  email: { type String, default: '' },
+  hashed_password: { type String, default: '' },
+  salt: { type String, default: '' }
+});
+
+UserSchema.
+
+mongoose.model('User', UserSchema)
