d3641121230a76e3e822fa27d505c09f56a03586
Russell Yeo <rusyeo@gmail.com>
2017-02-01 11:17:03 +0000

    install body-parser and mongodb

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, function(){
   console.log('listening on 3000');
 })
 
 app.get('/', function (request, response){
-  response.sendFile(__dirname + './index.html')
+  response.sendFile(__dirname + '/index.html');
+})
+
+app.post('/items', (request, response) => {
+  console.log(request.body);
 })
