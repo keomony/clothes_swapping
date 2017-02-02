b1de61907250ed34e1ae42b41340951996a43350
Russell Yeo <rusyeo@gmail.com>
2017-02-02 16:15:49 +0000

    fix production aws config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_region => 'eu-west-2',
-    :bucket => "clothes-swapping",
-    :s3_credentials => {:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+    :bucket => ENV.fetch('AWS_BUCKET'),
+    :s3_credentials => {:access_key_id => ENV.fetch('AWS_ACCESS_KEY_ID'),
+                        :secret_access_key => ENV.fetch('AWS_SECRET_ACCESS_KEY')}
   }
-  
+
 end
