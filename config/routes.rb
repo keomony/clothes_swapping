bcfa4b7c5ead4205d15f9deb25b20ae112b3dafd
Russell Yeo <rusyeo@gmail.com>
2017-02-04 16:13:46 +0000

    add link to my profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :items
   root "items#index"
 
-  get 'users/:id' => 'users#show'
+  get 'users/:id' => 'users#show', as: 'user_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
