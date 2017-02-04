001041269f7612af22657ce83ca8bafbc27711d9
Russell Yeo <rusyeo@gmail.com>
2017-02-04 13:39:53 +0000

    add user show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   root "items#index"
+
+  get 'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
