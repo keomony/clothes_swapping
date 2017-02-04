c3c1b26cdc8ea5aa6b6f69447fd5ae1ecc6dd75c
Russell Yeo <rusyeo@gmail.com>
2017-02-04 17:49:15 +0000

    change url helper to user_profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :items
   root "items#index"
 
-  get 'users/:id' => 'users#show', as: 'user_show'
+  get 'users/:id' => 'users#show', as: 'user_profile'
   get 'users/:id/wardrobe' => 'users#wardrobe', as: 'user_wardrobe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
