82894151ccb83a055d709eddbd003be73dd51c8a
agata-anastazja <agata.sumowska@gmail.com>
2017-02-06 18:12:53 +0000

    Update requester.rb

--- app/models/requester.rb
+++ app/models/requester.rb
@@ -1,5 +1,5 @@
 class Requester < ApplicationRecord
   belongs_to :user
   belongs_to :item
-  validates :user, uniqueness: { scope: :item, message: "has reviewed this restaurant already" }
+  validates :user, uniqueness: { scope: :item, message: "You already requested this item" }
 end
