31bf727091f1a6f2ccf6a460ac74d8c3390f19e4
Agata Sumowska <agata.sumowska@gmail.com>
2017-02-07 11:42:25 +0000

    Adds associations to user and item regarding selector

--- spec/models/requesters_spec.rb
+++ spec/models/requesters_spec.rb
@@ -3,5 +3,4 @@ require 'rails_helper'
 describe Requester, type: :model do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :item }
-  it { is_expected.to have_one :selector}
 end
