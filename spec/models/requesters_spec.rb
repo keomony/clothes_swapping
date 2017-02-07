b6699a7ed13ff9f5c3bc1b7b3ed8de4b8711d179
Agata Sumowska <agata.sumowska@gmail.com>
2017-02-07 11:16:48 +0000

    Updates simple config

--- spec/models/requesters_spec.rb
+++ spec/models/requesters_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe Requester, type: :model do
   it { is_expected.to belong_to :user }
-end
-describe Requester, type: :model do
   it { is_expected.to belong_to :item }
+  it { is_expected.to have_one :selector}
 end
