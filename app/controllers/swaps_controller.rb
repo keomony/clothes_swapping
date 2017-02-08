980422cbe9ac9a8874052853790239c42b885a76
keomony khun <keomony@gmail.com>
2017-02-08 15:24:49 +0000

    Show images in swap page

--- app/controllers/swaps_controller.rb
+++ app/controllers/swaps_controller.rb
@@ -1,7 +1,15 @@
 class SwapsController < ApplicationController
+
   def index
-    my_selectors = Selector.where(user_id: current_user.id)
+    cu_id = current_user.id
+    my_selectors = Selector.where(user_id: cu_id)
     my_selector_ids = my_selectors.ids
+
+    check_selector = Requester.where(user_id: cu_id)
+    selectors = Selector.where(requester_id: check_selector.ids)
     @swaps = Swap.where(selector_id: my_selector_ids)
+    @swaps += Swap.where(selector_id: selectors.ids)
+
   end
+
 end
--- spec/features/swap_feature_spec.rb
+++ spec/features/swap_feature_spec.rb
@@ -2,21 +2,34 @@ require 'rails_helper'
 
 describe "Swap" do
 
-  let(:user_1) { create(:user) }
-  let(:user_2) { create(:user, email: "testthisthing@test.com", id: '101') }
+  let(:mony) { create(:user) }
+  let(:jack) { create(:user, email: "testthisthing@test.com", id: '101') }
+  #
+  # let(:mony_item) {create(:item, user_id: mony.id, id: '200')}
+  # let(:jack_item) {create(:item, user_id: jack.id, color: 'blue', description: "Squirtle Onesie", id: "201")}
 
-  let(:item_1) {create(:item, user_id: user_1.id, id: '200')}
-  let(:item_2) {create(:item, user_id: user_2.id, color: 'blue', description: "Squirtle Onesie", id: "201")}
+  before do
+    login_as(jack, :scope => :user)
+    jack_item = create(:item, user_id: jack.id, color: 'blue', description: "Squirtle Onesie", id: "201")
+    sign_out
+    login_as(mony, :scope => :user)
+    mony_item = create(:item, user_id: mony.id, id: '200')
+    requester =  jack_item.requesters.create(item_id: jack_item.id, user_id: mony.id)
+    sign_out
+  end
 
-
-  subject(:request) { item_1.requesters.create(item_id: item_1.id, user_id: user_2.id) }
-  subject(:selectee) { Selector.create(item_id: item_2.id, user_id: user_1.id, requester_id: request.id ) }
-
-  scenario "build a selector" do
+  xscenario "build a selector" do
     expect(selectee).to be_a(Selector)
   end
 
   scenario "user should see both items" do
-    visit ''
+    login_as(jack, :scope => :user)
+    visit "/users/#{jack.id}"
+    click_link("Requests received")
+    click_link("Their wardrobe")
+    click_link("Request back")
+    visit "/users/#{jack.id}"
+    click_link("Swaps")
+    expect(page).to have_css("img[src*='pokemon_onesie.jpg']")
   end
 end
