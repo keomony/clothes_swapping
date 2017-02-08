9a7aa4126319ef83ffb025873331850a25e566c2
Agata Sumowska <agata.sumowska@gmail.com>
2017-02-08 11:11:06 +0000

    Adds selector to database, when you click button request back

--- app/controllers/selectors_controller.rb
+++ app/controllers/selectors_controller.rb
@@ -5,4 +5,18 @@ class SelectorsController < ApplicationController
     @requester = User.find(@request.user_id)
   end
 
+  def create
+    #maybe needs Requester.selector.create
+    @selector = Selector.create(user_id: params[:user_id],
+                                item_id: params[:item_id],
+                                requester_id: params[:requester_id])
+
+    redirect_to '/users/:id/profile/selectors/complete'
+  end
+
+  def show
+
+  end
+
+
 end
