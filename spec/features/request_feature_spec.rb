016b17fb2d7e1a5dd891c29b0955ceabc2a338c0
Russell Yeo <rusyeo@gmail.com>
2017-02-09 11:38:14 +0000

    refactor requests feature test

--- spec/features/request_feature_spec.rb
+++ spec/features/request_feature_spec.rb
@@ -2,36 +2,31 @@ require 'rails_helper'
 
 describe "Request" do
 
-  let(:user_1) { create(:user) }
-  let(:user_2) { create(:user, email: "testthisthing@test.com", id: '101') }
+  let(:agata) { create(:user) }
+  let(:russell) { create(:user, email: "testthisthing@test.com", id: '101') }
+  let(:agatas_item) {create(:item, user_id: agata.id)}
 
-  ## User 1 created an item
-  let(:item) {create(:item, user_id: user_1.id)}
-
-  ## User 2 requested that item
-  subject(:request) { item.requesters.create(item_id: item.id, user_id: user_2.id) }
+  subject(:request) { agatas_item.requesters.create(item_id: agatas_item.id, user_id: russell.id) }
 
   before do
-    login_as(user_2, :scope => :user)
+    login_as(russell, :scope => :user)
   end
 
   scenario "builds a request" do
     expect(request).to be_a(Requester)
   end
 
-  scenario "user makes a request for an item" do
-    visit "/items/#{item.id}"
+  scenario "russell makes a request for agatas item" do
+    visit "/items/#{agatas_item.id}"
     expect{click_button("Request")}.to change{Requester.count}.by(1)
   end
 
-  scenario "user can see the requests they have made" do
-    ## User 2  is logged in and requests user 1's item
-    visit "/items/#{item.id}"
+  scenario "agata can see the request russell has made" do
+    visit "/items/#{agatas_item.id}"
     click_button("Request")
     sign_out
-    ## User 1 logs in and sees user 2's request
-    login_as(user_1, :scope => :user)
-    visit "/users/#{user_1.id}/profile/requests"
-    expect(page).to have_content("#{user_2.email}")
+    login_as(agata, :scope => :user)
+    visit "/users/#{agata.id}/profile/requests"
+    expect(page).to have_content("#{russell.email}")
   end
 end
